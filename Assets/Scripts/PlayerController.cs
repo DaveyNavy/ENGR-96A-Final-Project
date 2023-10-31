6476ad0101be3d2e8708fd652b9085253dbca2a0
DaveyNavy <davidyzhu2@gmail.com>
2023-10-31 15:35:43 -0700

    Visual improvements, map collision detection

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,24 +9,38 @@ public class PlayerController : MonoBehaviour
 
     private float movementX = 0;
     private float movementY = 0;
+
+    Rigidbody2D rb;
+    List<RaycastHit2D> hits = new List<RaycastHit2D>();
+    public ContactFilter2D contactFilter;
+
     [SerializeField] GameObject bullet;
     void Start()
     {
         transform.position = Vector3.zero;
+        rb = GetComponent<Rigidbody2D>();  
+    }
+
+    private void FixedUpdate()
+    {
+        int count = rb.Cast(new Vector2(movementX, movementY), contactFilter, hits, speed * Time.deltaTime);
+        if (count == 0)
+        {
+            transform.position += speed * Time.deltaTime * (new Vector3(movementX, movementY, 0));
+        } 
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += speed * Time.deltaTime * (new Vector3(movementX, movementY, 0));
+        
     }
 
     void OnMove(InputValue value)
     {
-        Vector2 v = value.Get<Vector2>().normalized;
-
-        movementX = v.x;
-        movementY = v.y;
+        Vector2 movementVector = value.Get<Vector2>().normalized;
+        movementX = movementVector.x;
+        movementY = movementVector.y;
     }
 
     void OnFire(InputValue value)
